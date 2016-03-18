eb7b35bdba2af7caec039bbce1313e02c16b3f3d
Clive Seebregts <cseebregts@gmail.com>
2016-03-18 12:28:51 +0200

    Add initial register patient screen

--- src/app/src/main/java/org/jembi/icdm/ui/registerpatient/RegisterPatientActivity.java
+++ src/app/src/main/java/org/jembi/icdm/ui/registerpatient/RegisterPatientActivity.java
@@ -1,36 +1,35 @@
-package org.jembi.icdm.ui.viewpatients;
+package org.jembi.icdm.ui.registerpatient;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import org.jembi.icdm.R;
-import org.jembi.icdm.ui.registerpatient.RegisterPatientActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ViewPatientsActivity extends AppCompatActivity {
+public class RegisterPatientActivity extends AppCompatActivity {
     @Bind(R.id.toolbar) Toolbar mToolbar;
-    @Bind(R.id.fab) FloatingActionButton mButtonRegisterPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_patients_activity);
+        setContentView(R.layout.register_patient_activity);
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
 
-        mButtonRegisterPatient.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), RegisterPatientActivity.class));
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
-
 }
