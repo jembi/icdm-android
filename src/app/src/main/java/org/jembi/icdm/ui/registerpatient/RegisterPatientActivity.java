bf6ea0964b3716dfc9cf1087291e304a6f5eeb4a
Clive Seebregts <cseebregts@gmail.com>
2016-03-21 11:11:57 +0200

    Add register patient screen content (user interface widgets)

--- src/app/src/main/java/org/jembi/icdm/ui/registerpatient/RegisterPatientActivity.java
+++ src/app/src/main/java/org/jembi/icdm/ui/registerpatient/RegisterPatientActivity.java
@@ -1,11 +1,12 @@
 package org.jembi.icdm.ui.registerpatient;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import org.jembi.icdm.R;
 
@@ -14,6 +15,14 @@ import butterknife.ButterKnife;
 
 public class RegisterPatientActivity extends AppCompatActivity {
     @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.register_patient_edit_firstname) EditText mEditFirstName;
+    @Bind(R.id.register_patient_edit_lastname) EditText mEditLastName;
+    @Bind(R.id.register_patient_edit_id_number) EditText mEditIdNumber;
+    @Bind(R.id.register_patient_edit_mobile_number) EditText mEditMobileNumber;
+    @Bind(R.id.register_patient_edit_referral_reason) EditText mEditReferralReason;
+    @Bind(R.id.register_patient_text_dob) TextView mTextDateOfBirth;
+    @Bind(R.id.register_patient_button_dob) ImageButton mButtonDateOfBirth;
+    @Bind(R.id.register_patient_button_next) Button mButtonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +31,5 @@ public class RegisterPatientActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(mToolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
