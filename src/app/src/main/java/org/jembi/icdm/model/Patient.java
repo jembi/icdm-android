291bf5c69854f845163b7f5ad32a7a21b11a4acd
Clive Seebregts <cseebregts@gmail.com>
2016-03-24 15:20:44 +0200

    Sync patient model with dhis2 schema

--- src/app/src/main/java/org/jembi/icdm/model/Patient.java
+++ src/app/src/main/java/org/jembi/icdm/model/Patient.java
@@ -12,53 +12,112 @@ import java.util.Date;
 @Table(name = "Patients")
 public class Patient extends Model {
 
+    @Column(name = "TrackedEntityInstanceId")
+    public String mTrackedEntityInstanceId;
+
+    @Column(name = "TeuId")
+    public String mTeuId;
+
+    @Column(name = "IdNumber")
+    public String mNId;
+
     @Column(name = "FirstName")
     public String mFirstName;
 
     @Column(name = "LastName")
     public String mLastName;
 
-    @Column(name = "IdNumber")
-    public String mIdNumber;
-
     @Column(name = "DateOfBirth")
-    public Date mBirthDate;
+    public Date mDob;
 
     @Column(name = "Gender")
     public String mGender;
 
     @Column(name = "Mobile")
-    public String mMobile;
+    public String mFon;
+
+    @Column(name = "PharmStatus")
+    public String mPharmStatus;
+
+    @Column(name = "PharmId")
+    public String mPharmId;
+
+    @Column(name = "PharmName")
+    public String mPharmName;
+
+    @Column(name = "ConfirmationStatus")
+    public String mConfirmationStatus;
+
+    @Column(name = "MedPuInterval")
+    public String mMedPuInterval;
+
+    @Column(name = "NurseName")
+    public String mNurseName;
+
+    @Column(name = "NurseId")
+    public String mNurseId;
+
+    @Column(name = "Med1")
+    public String mMed1;
+
+    @Column(name = "Med2")
+    public String mMed2;
+
+    @Column(name = "Med3")
+    public String mMed3;
+
+    @Column(name = "Diagnosis")
+    public String mDiagnosis;
+
+    @Column(name = "ConfComment")
+    public String mConfComment;
 
     @Column(name = "ReferralReason")
     public String mReferralReason;
 
-    @Column(name = "ReferralCaseNumber")
-    public String mReferralCaseNumber;
+    @Column(name = "ChwStatus")
+    public String mChwStatus;
+
+    @Column(name = "ChwName")
+    public String mChwName;
 
     @Column(name = "ReferralDate")
     public Date mReferralDate;
 
-    @Column(name = "Status")
-    public String mStatus;
+    @Column(name = "ReferralNumber")
+    public String mReferralNumber;
+
+    @Column(name = "ChwId")
+    public String mChwId;
+
+    @Column(name = "NurseEventUid")
+    public String mNurseEventUid;
+
+    @Column(name = "ChwEventUid")
+    public String mChwEventUid;
+
+    @Column(name = "PharmEventUid")
+    public String mPharmEventUid;
+
+    @Column(name = "ChwxDate")
+    public Date mChwxDate;
+
+    @Column(name = "ConfxDate")
+    public Date mConfxDate;
+
+    @Column(name = "PharmxDate")
+    public Date mPharmxDate;
+
+    @Column(name = "ChwDueDate")
+    public Date mChwDueDate;
+
+    @Column(name = "ConfDueDate")
+    public Date mConfDueDate;
+
+    @Column(name = "PharmDueDate")
+    public Date mPharmDueDate;
 
     public Patient() {
         super();
-    }
-
-    public Patient(String firstName, String lastName, String idNumber, Date birthDate, String gender,
-                   String mobile, String referralReason, String referralCaseNumber,
-                   Date referralDate, String status) {
-        super();
-        mFirstName = firstName;
-        mLastName = lastName;
-        mIdNumber = idNumber;
-        mBirthDate = birthDate;
-        mGender = gender;
-        mMobile = mobile;
-        mReferralReason = referralReason;
-        mReferralCaseNumber = referralCaseNumber;
-        mReferralDate = referralDate;
-        mStatus = status;
     }
 }
