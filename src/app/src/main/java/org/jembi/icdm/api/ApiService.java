08b9ce718e5ee989ff633758f488a97b113a8a3a
Clive Seebregts <cseebregts@gmail.com>
2016-03-24 09:26:53 +0200

    Add correct versions of retrofit and gson converter and dealt with changes

--- src/app/src/main/java/org/jembi/icdm/api/ApiService.java
+++ src/app/src/main/java/org/jembi/icdm/api/ApiService.java
@@ -2,7 +2,8 @@ package org.jembi.icdm.api;
 
 import org.jembi.icdm.api.model.IcdmLineItems;
 
-import retrofit2.http.GET;
+import retrofit.Call;
+import retrofit.http.GET;
 
 /**
  * Created by Clive on 23 Mar 2016.
@@ -10,5 +11,5 @@ import retrofit2.http.GET;
 public interface ApiService {
     //http://107.170.125.158:8080/api/sqlViews/d0be6sHVvX2/data.json
     @GET("/api/sqlViews/d0be6sHVvX2/data.json")
-    public IcdmLineItems getPatients();
+    Call<IcdmLineItems> getPatients();
 }
--- src/app/src/main/java/org/jembi/icdm/app/AppApplication.java
+++ src/app/src/main/java/org/jembi/icdm/app/AppApplication.java
@@ -5,22 +5,26 @@ import com.activeandroid.ActiveAndroid;
 import org.jembi.icdm.R;
 import org.jembi.icdm.api.ApiService;
 
-import retrofit2.Retrofit;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
 
 /**
  * Created by Clive on 21 Mar 2016.
  */
 public class AppApplication extends com.activeandroid.app.Application {
+
     private ApiService mDhis2Service;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         ActiveAndroid.initialize(this);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(getString(R.string.dhis2_url))
-                        .build();
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         mDhis2Service = retrofit.create(ApiService.class);
     }
